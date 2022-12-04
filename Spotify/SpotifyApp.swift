b2adaa497b0857bbf163423834da22f46578bc70
Qkk49 <kapce4333@gmail.com>
2022-12-05 01:32:12 +0400

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Павел Клымко on 05.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
